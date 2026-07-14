c5c54e1f6172bfb21c7deb1d3f4efb7364bc9829
yummyzzzz <alex.christian.sr@gmail.com>
2026-07-14 23:11:50 +0200

    Add files via upload

---
+++ FilterSysVStudio/FilterSys/Helpers.h
@@ -0,0 +1,12 @@
+// Dialog
+#include "DialogHelper.hpp"
+
+// Get Contents
+#include "GetContents.hpp"
+
+// Display Filtered
+#include "DisplayFiltered.hpp"
+
+// Design
+#include "SysPause.hpp"
+#include "Symbols.hpp"
